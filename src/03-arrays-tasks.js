79832d40d1447612be4dfe5a7deafb9e51cb24f0
Gasanova Tamara <solas93@mail.ru>
2023-10-24 01:34:54 +0300

    feat: do getMovingSum task, array task

--- src/03-arrays-tasks.js
+++ src/03-arrays-tasks.js
@@ -248,8 +248,19 @@ function toArrayOfSquares(arr) {
  *   [ 0, 0, 0, 0, 0]         => [ 0, 0, 0, 0, 0]
  *   [ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 ] => [ 1, 3, 6, 10, 15, 21, 28, 36, 45, 55 ]
  */
-function getMovingSum(/* arr */) {
-  throw new Error('Not implemented');
+function getMovingSum(arr) {
+  const resultArr = [];
+
+  function getSum(item, element) {
+    let resultItem = item;
+    resultItem += element;
+    resultArr.push(resultItem);
+    return resultItem;
+  }
+
+  arr.reduce(getSum, 0);
+
+  return resultArr;
 }
 
 /**
